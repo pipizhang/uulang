55a7f1fba02752b5b98673890378f0f8171965b4
peterzhang <zhh520@gmail.com>
2019-04-20 22:29:46 +0200

    [UPD] Add global opencc instance

--- opencc.go
+++ opencc.go
@@ -4,12 +4,25 @@ import (
 	"github.com/liuzl/gocc"
 )
 
+var (
+	s2t *gocc.OpenCC
+	t2s *gocc.OpenCC
+)
+
+func init() {
+	var err error
+	s2t, err = gocc.New("s2t")
+	if err != nil {
+		panic("Failed to load OpenCC(s2t)")
+	}
+	t2s, err = gocc.New("t2s")
+	if err != nil {
+		panic("Failed to load OpenCC(t2s)")
+	}
+}
+
 // S2T Simplified Chinese to Traditional Chinese
 func S2T(in string) (string, error) {
-	s2t, err := gocc.New("s2t")
-	if err != nil {
-		return "", err
-	}
 	out, err := s2t.Convert(in)
 	if err != nil {
 		return "", err
@@ -19,11 +32,7 @@ func S2T(in string) (string, error) {
 
 // T2S Traditional Chinese to Simplified Chinese
 func T2S(in string) (string, error) {
-	s2t, err := gocc.New("t2s")
-	if err != nil {
-		return "", err
-	}
-	out, err := s2t.Convert(in)
+	out, err := t2s.Convert(in)
 	if err != nil {
 		return "", err
 	}

faf8029c7b26d7f657beb459051447e2787984ba
peterzhang <zhh520@gmail.com>
2019-04-27 16:11:35 +0200

    [ADD] Add Pinyin Tome nubmer

--- seg.go
+++ seg.go
@@ -2,7 +2,8 @@ package uulang
 
 import (
 	"github.com/huichen/sego"
-	"strings"
+	"regexp"
+	"strconv"
 )
 
 var segmenter sego.Segmenter
@@ -13,10 +14,11 @@ func init() {
 
 type SegWord struct {
 	Id      int
-	Word    string `json:"word"`
-	Pos     string `json:"pos"`
-	Pinyin  string `json:"pinyin"`
-	PinyinN string `json:"pinyin_n"`
+	Word    string   `json:"word"`
+	Pos     string   `json:"pos"`
+	Pinyin  []string `json:"pinyin"`
+	PinyinN []string `json:"pinyin_n"`
+	PinyinT []int    `json:"pinyin_t"`
 }
 
 type SegWords []*SegWord
@@ -47,14 +49,24 @@ func Cut(text string) *SegWords {
 func CutWithPinyin(text string) *SegWords {
 	btext := []byte(text)
 	segs := segmenter.Segment(btext)
+	re := regexp.MustCompile(`(\d)+`)
 
 	var segWords SegWords
 	{
 		for k, seg := range segs {
 			_text := seg.Token().Text()
 			_pos := seg.Token().Pos()
-			_py := strings.Join(LazyPinyinTone(_text), " ")
-			_pyn := strings.Join(LazyPinyinTone2(_text), " ")
+			//_py := strings.Join(LazyPinyinTone(_text), " ")
+			//_pyn := strings.Join(LazyPinyinTone2(_text), " ")
+			_py := LazyPinyinTone(_text)
+			_pyn := LazyPinyinTone2(_text)
+			_pyt := []int{}
+
+			for _, _tmp := range _pyn {
+				m := re.FindString(_tmp)
+				i, _ := strconv.Atoi(m)
+				_pyt = append(_pyt, i)
+			}
 
 			segWords = append(segWords, &SegWord{
 				Id:      k,
@@ -62,6 +74,7 @@ func CutWithPinyin(text string) *SegWords {
 				Pos:     _pos,
 				Pinyin:  _py,
 				PinyinN: _pyn,
+				PinyinT: _pyt,
 			})
 		}
 	}
